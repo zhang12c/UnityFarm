10f5595b9f17836b8700ceaa47287d8f19a29f36
jetkeey <jetkeey0317@gmail.com>
2023-03-16 00:34:23 +0800

    使用 cinemachine confiner 限制摄像机范围

---
+++ Assets/Scripts/Until/SwitchBounds.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+public class SwitchBounds : MonoBehaviour
+{
+    private CinemachineConfiner ccr;
+    private PolygonCollider2D _polygonCollider2D;
+
+    private void Awake()
+    {
+        ccr = GetComponent<CinemachineConfiner>();
+    }
+    // TODO : 应该在切换场景的时候调用
+    private void Start()
+    {
+        SwitchConfinerShape();
+    }
+
+    private void SwitchConfinerShape()
+     {
+         _polygonCollider2D = GameObject.FindWithTag("BoundsConfiner")?.GetComponent<PolygonCollider2D>();
+         if (_polygonCollider2D)
+         {
+             ccr.m_BoundingShape2D = _polygonCollider2D;
+             // 切换参加的时候可能边界失效，要用这个方法清一下缓存
+             ccr.InvalidatePathCache();
+         }
+         else
+         {
+             Debug.LogError("获得边界Bounds失败了");
+         }
+     }
+}
