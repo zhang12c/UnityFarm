c0d3c008eff9050886509285326b32f8b0def672
jetkeey <jetkeey0317@gmail.com>
2023-04-07 23:00:32 +0800

    提交保存游戏进度的UI

---
+++ Assets/Scripts/Menu/SaveSlotUI.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.UI;
+namespace Menu
+{
+    public class SaveSlotUI : MonoBehaviour
+    {
+        public Text dataTime, dataScene;
+        
+        private Button _currentBtn;
+
+        /// <summary>
+        /// 在父物体中的索引
+        /// </summary>
+        private int Index
+        {
+            get
+            {
+                return transform.GetSiblingIndex();
+            }
+        }
+
+        private void Awake()
+        {
+            _currentBtn = GetComponent<Button>();
+            _currentBtn.onClick.AddListener(LoadGameData);
+        }
+
+        private void LoadGameData()
+        {
+            Debug.Log(Index);
+        }
+    }
+}
