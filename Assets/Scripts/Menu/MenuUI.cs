4ab857779d83aca2ed86bf7ef1f4b1eb05caee6f
jetkeey <jetkeey0317@gmail.com>
2023-04-07 17:52:42 +0800

    主菜单

---
+++ Assets/Scripts/Menu/MenuUI.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+namespace Menu
+{
+    public class MenuUI : MonoBehaviour
+    {
+        public GameObject[] panels;
+
+        public void SwitchPanel(int index)
+        {
+            for (int i = 0; i < panels.Length; i++)
+            {
+                if (i == index)
+                {
+                    panels[i].transform.SetAsLastSibling();
+                }
+            }
+        }
+
+        public void ExitGame()
+        {
+            Application.Quit();
+        }
+    }
+}
